d8c8f41247b64d4ca7c4a795547ef857bf133d22
shenmengjia <shenmj13@gmail.com>
2022-05-10 15:35:26 +0800

    update

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 # execute PYTHONPATH=. python -m crawler.main --help 
-PYTHONPATH=. python -m crawler.main "graph" --to_translate True --gap 1
-PYTHONPATH=. python -m crawler.main "time series" --to_translate True --gap 1
+PYTHONPATH=. python -m crawler.main "graph" --to_translate True --gap 3
+PYTHONPATH=. python -m crawler.main "time series" --to_translate True --gap 3
